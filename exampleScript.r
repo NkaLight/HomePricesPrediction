9b9805813e9cf435a7ce1d3d201134e64dce0fa9
NkaLight <nkanyisoowethu@gmail.com>
2024-01-12 00:32:52 +0200

    startpage UI functionality

--- exampleScript.r
+++ exampleScript.r
@@ -4,6 +4,7 @@ if (!requireNamespace("jsonlite", quietly = TRUE)) install.packages("jsonlite")
 install.packages("jsonlite")
 # prediction_script.R
 
+Sys.setenv(R_LIBS_USER = "C:/Program Files/R/R-4.3.1/library")
 
 
 # Read the input value from the command line arguments
@@ -17,3 +18,8 @@ response <- list(prediction = transformed_value)
 
 # Print the JSON response to the console
 cat(toJSON(response), "\n")
+
+
+
+#binary packages are in C:\Users\nkany\AppData\Local\Temp\RtmpiSb6Gj\downloaded_packages
+#.libsPath("jsonite") = C:/Program Files/R/R-4.3.1/library
